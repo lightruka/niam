75c1cbb478d88c90c3bdeacf564b9d18a9353ebe
lightruka <ruka64073@gmail.com>
2026-04-16 19:05:50 +0200

    Update API

--- api/generate.js
+++ api/generate.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
           "courses_manquantes": ["ingrédient 1", "ingrédient 2"]
         }`;
 
-        const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+        const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
 
         const response = await fetch(url, {
             method: "POST",
